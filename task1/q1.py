14ef1a1a7380a3bcfa64125288e908e717da2633
Muhir Kapoor <muhir.mitmpl2024@learner.manipal.edu>
2025-09-23 17:07:13 +0530

    Refactor character counting in dictionary

--- task1/q1.py
+++ task1/q1.py
@@ -9,7 +9,7 @@ for i in range(0,n):
 for i in all_lowercase_letters:
     dictionary[i]=0
 for j in strings:
-    for i in all_lowercase_letters:
-        dictionary[i]=j.count(i)+dictionary[i]
+    for i in j:
+        dictionary[i]=1+dictionary[i]
 print(dictionary)
             
