00bbcf5f279094cfa78a2415d779be85f76d035c
Muhir Kapoor <muhir.mitmpl2024@learner.manipal.edu>
2025-09-24 18:37:04 +0530

    Update q3_binarysearch.py

--- task2/q3_binarysearch.py
+++ task2/q3_binarysearch.py
@@ -18,8 +18,12 @@ def find_insertion_point(arr, target):
         mid = low + (high - low) // 2
         if arr[mid] == target:
             return mid
-        elif mid + 1 < len(arr) and mid - 1 >= 0 and arr[mid + 1] > target and arr[mid - 1] < target:
+        elif  mid - 1 >= 0 and arr[mid] > target and arr[mid - 1] < target:
             return mid
+        elif mid+1>=len(arr) and arr[mid]<target:
+            return (mid+1)
+        elif mid-1<0 and arr[mid]>target:
+            return 0
         elif arr[mid] < target:
             low = mid + 1
         else:
@@ -30,9 +34,11 @@ def add(value):
     index = hash_function(value)
     if hash_table[index] == []:
         hash_table[index].append(value)
+        print(hash_table)
     else:
         index2 = find_insertion_point(hash_table[index], value)
         hash_table[index].insert(index2, value)
+        print(hash_table)
 
 for i in loi:
     add(i)
